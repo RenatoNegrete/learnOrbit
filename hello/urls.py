ab7e9dec2ab7c3b68b493c520b3462bd270f5612
Renato Negrete <negreterenato@gmail.com>
2024-10-22 09:51:43 -0500

    acabar login y comenzar con perfil

--- hello/urls.py
+++ hello/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("login/", views.login, name="login")
+    path("login/", views.login, name="login"),
+    path("login_continue/", views.loginContinue, name="loginContinue"),
+    path('redirect/', views.redirect_view, name='redirect_view'),
+    path("home/", views.home, name="home"),
+    path("perfil/", views.perfil, name="perfil")
 ]
--- hello/views.py
+++ hello/views.py
@@ -16,10 +16,38 @@ def login(request):
             # Hardcoded username and password check
             if username == 'user' and password == '12345':
                 # Simulate successful login
-                return redirect('index')  # Redirect to a success page
+                return redirect('home')  # Redirect to a success page
             else:
                 form.add_error(None, 'Invalid username or password.')
     else:
         form = LoginForm()
     
     return render(request, 'hello/login.html', {'form': form})
+
+def loginContinue(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            
+            # Hardcoded username and password check
+            if username == 'user' and password == '12345':
+                # Simulate successful login
+                return redirect('home')  # Redirect to a success page
+            else:
+                form.add_error(None, 'Invalid username or password.')
+    else:
+        form = LoginForm()
+    
+    return render(request, 'hello/login_continue.html', {'form': form})
+
+def redirect_view(request):
+    # Any custom logic before redirecting can be added here
+    return redirect('loginContinue')  # Redirect to the 'courses' view
+
+def home(request):
+    return render(request, "hello/home.html")
+
+def perfil(request):
+    return render(request, "hello/perfil.html")
